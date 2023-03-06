11e26eaee4f4e3ff4c6ae3120d794897ae780938
adriancauner <adriannsamuelcauner@gmail.com>
2023-03-06 11:41:02 +0000

    Salvar

---
+++ IMC.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class IMC {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        double altura,peso,imc;
+        
+        System.out.println("Digite seu peso (em kg): ");
+        peso = input.nextDouble();
+        System.out.println("Digite a sua altura  (em metros): ");
+        altura = input.nextDouble();
+        
+        imc = peso / (altura * altura);
+
+        
+        if (imc < 18.5) {
+            System.out.printf("O IMC da pessoa é " + imc + "\n faixa de peso  é : Abaixo do peso ta só o osso \n");
+        } else if (imc >= 18.5 $$ imc < 24.9 ) {
+            System.out.printf("O IMC da pessoa é " + imc + "\n faixa de peso  é : Peso Normal parabéns você normal\n");
+        } else if (imc >=25.0 $$ imc <= 29.9) {
+            System.out.printf("O IMC da pessoa é " + imc + "\n faixa de peso  é : Sobre peso perde uns quilos né meu nobre \n");
+        } else if (imc >=30.0 $$ imc<= 34.9) {
+            System.out.printf("O IMC da pessoa é " + imc + "\n faixa de peso  é : Obesidade grau  tipo meteoro\n");
+        } else if (imc >= 35.0 && imc <= 39.9) {
+            System.out.printf("O IMC da pessoa é " + imc + "\n faixa de peso  é : Obesidade grau   thaisplodindo \n");
+        } else if (imc > 40.0) {
+            System.out.printf("O IMC da pessoa é " + imc + "\n faixa de peso  é : Obesidade grau  tipo thais carla \n");
+        }
+    }
+}
